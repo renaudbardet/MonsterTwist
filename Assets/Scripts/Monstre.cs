2a49736b80047e9c99a00aac7cb6e7f1e59155e1
Antoineabel <antoien@live.fr>
2015-05-09 01:07:30 +0200

    Gros changements

---
+++ Assets/Scripts/Monstre.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Monstre : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D collision) {
+		
+			Debug.Log (collision.gameObject.name );
+		
+	}
+
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+	private Vector3 movementVector;
+	//private CharacterController characterController;
+	private float movementSpeed = 10;
+
+	public int joystickNumber;
+
+	// Use this for initialization
+	void Start () {
+		//movementVector.y = transform.position.y;
+		//characterController = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		string joystickString = joystickNumber.ToString();
+
+		movementVector.x = Input.GetAxis("Horizontal_P" + joystickString) * movementSpeed;
+		movementVector.z = 0;
+		movementVector.y = - Input.GetAxis("Vertical_P" + joystickString) * movementSpeed;
+
+		GetComponent<Rigidbody2D>().velocity = movementVector;
+		// rigid remplacer par cible methode target_input
+
+		//GetComponent<Rigidbody2D>().AddForce(movementVector);
+
+		//transform.position = movementVector;
+		//characterController.Move(movementVector * Time.deltaTime);
+	}
+
+}
