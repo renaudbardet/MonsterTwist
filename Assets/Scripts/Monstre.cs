476aac0a588988d04b9ed1de2d88f31514f061fd
renaud <renaud.bardet@gmail.com>
2015-05-09 13:49:16 +0200

    added arrows

--- Assets/Scripts/Monstre.cs
+++ Assets/Scripts/Monstre.cs
@@ -16,8 +16,24 @@ public class Monstre : MonoBehaviour {
 	}
 
 	void OnCollisionEnter2D(Collision2D collision) {
+
+		PlayerController playerHitting = null;
+
+		if (collision.gameObject.GetComponent<PlayerMovement> () != null) {
+			Debug.Log ("monster hit by player");
+			playerHitting = collision.gameObject.GetComponent<PlayerMovement> ().controller;
+		}
 		
-		GameManager.instance.PlayerSlaynMonster (collision.gameObject.GetComponent<PlayerMovement>().controller);
+		if (collision.gameObject.GetComponent<Arrow> () != null) {
+			Debug.Log("monster hit by arrow");
+			playerHitting = collision.gameObject.GetComponent<Arrow> ().owner;
+			Debug.Log(playerHitting);
+		}
+
+		Debug.Log(playerHitting);
+
+		if (playerHitting != null)
+			GameManager.instance.PlayerSlaynMonster (playerHitting);
 		
 	}
 
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,16 +4,20 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
 	public GameObject playerGraphic;
+	public Rigidbody2D arrow;
 	private Vector3 movementVector;
-	//private CharacterController characterController;
 	private float movementSpeed = 10;
+
+	public double arrowCooldown = .1;
+	private double lastArrowShot = 0;
 
 	public int joystickNumber;
 
+	Orient currentHeading = Orient.Right;
+
 	// Use this for initialization
 	void Start () {
-		//movementVector.y = transform.position.y;
-		//characterController = GetComponent<CharacterController>();
+		playerGraphic.GetComponent<PlayerMovement>().controller = this;
 	}
 	
 	// Update is called once per frame
@@ -21,11 +25,69 @@ public class PlayerController : MonoBehaviour {
 
 		string joystickString = joystickNumber.ToString();
 
-		movementVector.x = Input.GetAxis("Horizontal_P" + joystickString) * movementSpeed;
+		float xAxis = Input.GetAxis("Horizontal_P" + joystickString) * movementSpeed;
+		float yAxis = - Input.GetAxis("Vertical_P" + joystickString) * movementSpeed;
+		movementVector.x = xAxis;
 		movementVector.z = 0;
-		movementVector.y = - Input.GetAxis("Vertical_P" + joystickString) * movementSpeed;
+		movementVector.y = yAxis;
+
+		float xAxisDelta = Mathf.Abs (xAxis);
+		float yAxisDelta = Mathf.Abs (yAxis);
+
+		if (xAxisDelta > yAxisDelta) {
+
+			if (xAxis < 0) {
+				setHeading (Orient.Left);
+			} else if (xAxis > 0) {
+				setHeading (Orient.Right);
+			}
+
+		} else {
+
+			if (yAxis < 0) {
+				setHeading (Orient.Up);
+			} else if (yAxis > 0) {
+				setHeading (Orient.Down);
+			}
+
+		}
 
 		playerGraphic.GetComponent<Rigidbody2D>().velocity = movementVector;
+
+		if (	Input.GetButton ("Fire_P" + joystickString) 
+		    	&& (Time.time - lastArrowShot) > arrowCooldown
+		    ) {
+
+			Rigidbody2D a = Instantiate( arrow, playerGraphic.transform.position, transform.rotation ) as Rigidbody2D;
+
+			a.GetComponent<Arrow>().owner = this;
+
+			switch( currentHeading ){
+			case Orient.Up:
+				a.velocity = new Vector3( 0, -10 );
+				break;
+			case Orient.Down:
+				a.velocity = new Vector3( 0, 10 );
+				break;
+			case Orient.Left:
+				a.velocity = new Vector3( -10, 0 );
+				break;
+			case Orient.Right:
+				a.velocity = new Vector3( 10, 0 );
+				break;
+			}
+
+			lastArrowShot = Time.time;
+
+		}
+
+	}
+	
+	void setHeading( Orient newHeading ) {
+
+		if (newHeading != currentHeading) {
+			currentHeading = newHeading;
+		}
 
 	}
 
